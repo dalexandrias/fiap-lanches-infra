b33da22c181b0479df71d2f12509e532c55cc7aa
Davi Alexandrias de Oliveira <dalexandrias@hotmail.com>
2023-11-07 22:44:36 -0300

    Alteracao da subnet

--- ecs_template.tf
+++ ecs_template.tf
@@ -50,7 +50,7 @@ resource "aws_lb" "ecs_alb" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.default.id]
-  subnets            = [aws_subnet.subnet[count.index].id, aws_subnet.subnet2[count.index].id]
+  subnets            = [element(aws_subnet.subnet.*.id, 0), element(aws_subnet.subnet.*.id, 1)]
 
   tags = {
     Name = "ecs-alb"
