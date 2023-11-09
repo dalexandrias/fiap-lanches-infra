8cb5c8d4932170cfdaf360e2e41b8e25be29a6f2
Davi Alexandrias <77761467+dalexandrias@users.noreply.github.com>
2023-11-08 21:54:40 -0300

    Update ecs_template.tf

--- ecs_template.tf
+++ ecs_template.tf
@@ -59,7 +59,7 @@ resource "aws_lb" "ecs_alb" {
 
 resource "aws_lb_listener" "ecs_alb_listener" {
   load_balancer_arn = aws_lb.ecs_alb.arn
-  port              = 8080
+  port              = 80
   protocol          = "HTTP"
 
   default_action {
@@ -70,7 +70,7 @@ resource "aws_lb_listener" "ecs_alb_listener" {
 
 resource "aws_lb_target_group" "ecs_tg" {
   name        = "${var.environment}-ecs-target-group"
-  port        = 80
+  port        = 8080
   protocol    = "HTTP"
   target_type = "ip"
   vpc_id      = aws_vpc.vpc.id
