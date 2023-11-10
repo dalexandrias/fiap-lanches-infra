a4ce2acb2b37170419dcda9d7efe84d2a220af44
Davi Alexandrias de Oliveira <dalexandrias@hotmail.com>
2023-11-09 22:05:50 -0300

    Correcao na subnet do ecs service

--- ecs_template.tf
+++ ecs_template.tf
@@ -27,7 +27,7 @@ resource "aws_launch_template" "ecs_lt" {
 }
 
 resource "aws_autoscaling_group" "ecs_asg" {
-  vpc_zone_identifier = [element(aws_subnet.private_subnet.*.id, 0)]
+  vpc_zone_identifier = [element(aws_subnet.subnet2.*.id, 0)]
   desired_capacity    = 2
   max_size            = 3
   min_size            = 1
