07d33398fad6f79429a632896f497c2bfe91d5fb
Davi Alexandrias de Oliveira <dalexandrias@hotmail.com>
2024-01-27 00:01:33 -0300

    Delete

--- logs.tf
+++ logs.tf
@@ -2,11 +2,20 @@
 
 # Set up CloudWatch group and log stream and retain logs for 30 days
 resource "aws_cloudwatch_log_group" "fiap_lanches_conta_log_group" {
-  name              = "/ecs/${var.app_name}-conta-app"
+  name              = "/ecs/${var.container_conta_name}"
   retention_in_days = 30
 
   tags = {
-    Name = "${var.app_name}-log-group"
+    Name = "${var.container_conta_name}-log-group"
+  }
+}
+
+resource "aws_cloudwatch_log_group" "fiap_lanches_product_log_group" {
+  name              = "/ecs/${var.container_product_name}"
+  retention_in_days = 30
+
+  tags = {
+    Name = "${var.container_product_name}-log-group"
   }
 }
 
